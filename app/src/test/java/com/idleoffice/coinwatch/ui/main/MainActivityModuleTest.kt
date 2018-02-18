bdd5535a82e1e8562cabac608b99074cafec2944
dan-0 <dan.lowe002@gmail.com>
2018-02-17 22:13:42 -0500

    Supports #4. Added tests for MainActivityModule

---
+++ app/src/test/java/com/idleoffice/coinwatch/ui/main/MainActivityModuleTest.kt
@@ -0,0 +1,34 @@
+package com.idleoffice.coinwatch.ui.main
+
+import com.idleoffice.coinwatch.MainApp
+import com.idleoffice.coinwatch.data.model.bci.BitcoinAverageInfoService
+import com.idleoffice.coinwatch.rx.SchedulerProvider
+import com.nhaarman.mockito_kotlin.mock
+import org.junit.Test
+
+/**
+ * Mostly just a stub test class to assert that we're not experiencing odd underlying issues
+ */
+class MainActivityModuleTest {
+
+    private var subject = MainActivityModule()
+
+    private var mockApp = mock<MainApp>()
+    private var mockSchedulerProvider = mock<SchedulerProvider>()
+    private var mockBitcoinAverageInfoService = mock<BitcoinAverageInfoService>()
+    private var mockMainViewModel = mock<MainViewModel>()
+
+    @Test
+    fun provideMainViewModel() {
+        subject.provideMainViewModel(
+                mockApp,
+                mockSchedulerProvider,
+                mockBitcoinAverageInfoService)
+    }
+
+    @Test
+    fun mainViewModelProvider() {
+        subject.mainViewModelProvider(mockMainViewModel)
+    }
+
+}
